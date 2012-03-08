fea47e7e579e81a4ac82b71028a7537752595885
Yaroslav Yarigin <luminarior@gmail.com>
2012-03-09 02:24:16 +0400

    Import net-p2p/opentracker from Gentoo Bugzilla

---
+++ net-p2p/opentracker/files/opentracker.init.d
@@ -0,0 +1,29 @@
+#!/sbin/runscript
+# Copyright 2011 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+BINFILE="/usr/bin/opentracker"
+PIDFILE="/var/run/opentracker.pid"
+
+depend() {
+	use net
+}
+
+start() {
+    ebegin "Starting Opentracker daemon"
+    start-stop-daemon \
+	--background \
+	--start --make-pidfile --pidfile "$PIDFILE" \
+	--exec "$BINFILE" -- \
+	${EXEC_PARAMS}
+    eend $?
+}
+
+stop() {
+    ebegin "Stopping Opentracker daemon"
+    start-stop-daemon \
+	--stop --pidfile "$PIDFILE" \
+	--exec "$BINFILE"
+    eend $?
+}
+
